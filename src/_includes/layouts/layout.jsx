c45f09767ebbcbf79b843c937909373bb906b5da
fabon <syobon.hinata.public@gmail.com>
2021-05-29 15:37:52 +0900

    Add description in head

--- src/_includes/layouts/layout.jsx
+++ src/_includes/layouts/layout.jsx
@@ -1,9 +1,10 @@
-export default ({ children, title }) =>
+export default ({ children, title, description }) =>
 <html lang="ja">
 <head>
     <meta charSet="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+    <meta name="description" content={description || "ふぁぼんのホームページ"} />
     <link rel="icon" href="/favicon.ico" />
     <link rel="stylesheet" href="/css/common.css" />
     <title>{title || "ふぁぼんのホームページ"}</title>
