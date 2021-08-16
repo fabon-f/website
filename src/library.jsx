5ccf0e5e694d1e47c254acd74a5b6211ac490b7e
fabon <syobon.hinata.public@gmail.com>
2021-08-17 02:36:05 +0900

    Update

--- src/library.jsx
+++ src/library.jsx
@@ -7,6 +7,13 @@ export default data =>
     <main>
         <h1>駄文図書館</h1>
 
+        <h2>多少有意義なやつ</h2>
+        <p>まあ、あくまで「多少」なんですが。</p>
+        <ul>
+            <li>2020年4月15日、コロナ禍の長期化という当たってしまった悲観的予測と絶望の話 <ExternalLink href="https://yuyusuki.hatenablog.com/entry/2020/04/15/012116">気軽な海外旅行という、かつてあったはずの幻影のこと</ExternalLink></li>
+            <li>「ふぁぼんはこんな本読んでそう」という偏見のコレクション <ExternalLink href="https://yuyusuki.hatenablog.com/entry/2021/07/25/172939">「偏見本棚」をやってみた</ExternalLink></li>
+        </ul>
+
         <h2>エモの押し売り文庫</h2>
         <ul>
             <li>小学生の頃のちっぽけな願いと、梅田貨物線と、巡り合わせ <ExternalLink href="https://yuyusuki.hatenablog.com/entry/2018/03/14/164248">快速御坊行きと私</ExternalLink></li>
@@ -20,6 +27,12 @@ export default data =>
             <li>小説中毒の私はいかにしてオタクになったか、あるいは私とエロゲソングのファーストコンタクト <ExternalLink href="https://yuyusuki.hatenablog.com/entry/2018/04/30/000956">読書遍歴とSSとオタクの目覚め</ExternalLink></li>
             <li>私がロシア語に出会った日 <ExternalLink href="https://yuyusuki.hatenablog.com/entry/2020/06/17/183743">学士の異常な愛情 または私は如何にして心配するのを止めてロシア語を愛するようになったか</ExternalLink></li>
             <li>新元号「令和」発表記念、クール美少女とゆるふわ美少女の百合の尊さに関する考察 <ExternalLink href="https://yuyusuki.hatenablog.com/entry/2019/04/01/232956">私が特別尊いと思うキャラクター(特に女の子同士)の組み合わせについて</ExternalLink></li>
+        </ul>
+        <h2>謎</h2>
+        <p>怪文書などその他どうしようもない文章たち</p>
+        <ul>
+            <li>ふぁぼん抱腹絶倒クッキング <ExternalLink href="https://yuyusuki.hatenablog.com/entry/2020/05/08/230205">卵なんか嫌いだ</ExternalLink></li>
+            <li><ExternalLink href="https://yuyusuki.hatenablog.com/entry/2021/08/04/001102">【豊田商事】オイショ！ロシア語訳【この世の終わりみたいな朝礼】</ExternalLink></li>
         </ul>
         <a href="/">トップへ</a>
     </main>;
