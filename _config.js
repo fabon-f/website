077354b9724759529dac600200a9b3df9fdae428
fabon <syobon.hinata.public@gmail.com>
2021-08-17 02:35:45 +0900

    Update lume

--- _config.js
+++ _config.js
@@ -1,5 +1,5 @@
-import lume from "lume/mod.js";
-import jsx from "lume/plugins/jsx.js";
+import lume from "lume/mod.ts";
+import jsx from "lume/plugins/jsx.ts";
 
 const site = lume({
     src: "src",
