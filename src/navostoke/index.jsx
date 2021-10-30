f3883f7d07b04848935bf95c0056664e6bb2c053
fabon <syobon.hinata.public@gmail.com>
2021-10-31 06:50:40 +0900

    Add circle page

---
+++ src/navostoke/index.jsx
@@ -0,0 +1,25 @@
+import { ExternalLink } from "../../lib/components.tsx"
+
+export const title = "サークル「ナヴァストーケ」"
+export const description = "ふぁぼんの個人サークル「ナヴァストーケ」の公式サイト"
+
+export default data => {
+    const books = data.series.filter(series => Array.isArray(series.books)).map(series =>
+        <div>
+            <h3>{series.name}</h3>
+            {series.description && <p>{series.description}</p>}
+            {series.books && series.books.map(book => <div><a href={book.id}>{book.name}</a></div>)}
+        </div>
+    )
+    return <main role="main">
+        <h1>サークル「ナヴァストーケ」</h1>
+        <p>ふぁぼんの個人サークルです。科学、コンピュータ、ロシア語、旧ソ連、鉄道や船やバスや旅行など、節操なしにいろいろ書きます。</p>
+        <p>評論・情報系の本がメインですが、一次/二次創作の小説も出すかもしれません。</p>
+        <p>※サークル名の「ナヴァストーケ」はロシア語で「на востоке」すなわち「東で」を意味します。</p>
+
+        <ExternalLink href="https://navostoke.booth.pm">BOOTH(通販)</ExternalLink>
+
+        <h2>作品一覧</h2>
+        {books}
+    </main>
+}
---
+++ src/navostoke/russian-police-1.jsx
@@ -0,0 +1,9 @@
+import { ExternalLink } from "../../lib/components.tsx"
+
+export const title = "ロシア語警察24時 vol. 1 人名エトセトラ"
+export const description = "「ロシア語警察24時 vol. 1 人名エトセトラ」の紹介ページ"
+
+export default data =>
+    <main role="main">
+        <h1>ロシア語警察24時 vol. 1 人名エトセトラ</h1>
+    </main>
