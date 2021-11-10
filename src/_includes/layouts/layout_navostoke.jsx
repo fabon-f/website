d0b50b06267fd536504f59d46035983ea04285df
fabon <syobon.hinata.public@gmail.com>
2021-11-11 03:31:50 +0900

    Update copyright year dynamically

--- src/_includes/layouts/layout_navostoke.jsx
+++ src/_includes/layouts/layout_navostoke.jsx
@@ -2,7 +2,7 @@ import defaultLayout from "./layout.jsx";
 
 export default ({ children, ...props }, filters) => {
     const footer = <footer id="footer">
-        <p>© 2021- ナヴァストーケ «на востоке»</p>
+        <p>© 2021-{new Date().getFullYear()} ナヴァストーケ «на востоке»</p>
     </footer>;
     return defaultLayout({ children: [children, footer], ...props }, filters);
 }
