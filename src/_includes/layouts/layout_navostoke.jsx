58f8b3555583d7e5a486c31ec189ccb40aa627f2
fabon <syobon.hinata.public@gmail.com>
2021-11-11 08:27:10 +0900

    Update footer

--- src/_includes/layouts/layout_navostoke.jsx
+++ src/_includes/layouts/layout_navostoke.jsx
@@ -1,8 +1,11 @@
 import defaultLayout from "./layout.jsx";
 
 export default ({ children, ...props }, filters) => {
-    const footer = <footer id="footer">
-        <p>© 2021-{new Date().getFullYear()} ナヴァストーケ «на востоке»</p>
+    const footer = <footer>
+        <p><a href="/navostoke/">サークルのトップページ</a> <a href="/">ふぁぼんのホームページ</a></p>
+        <div id="footer">
+            <p>© 2021-{new Date().getFullYear()} ナヴァストーケ «на востоке»</p>
+        </div>
     </footer>;
     return defaultLayout({ children: [children, footer], ...props }, filters);
 }
