c3beffe79a0209dddb4a08230ce29d05be261213
fabon <syobon.hinata.public@gmail.com>
2021-11-11 16:56:38 +0900

    Add header

--- src/_includes/layouts/layout_navostoke.jsx
+++ src/_includes/layouts/layout_navostoke.jsx
@@ -1,12 +1,24 @@
 import defaultLayout from "./layout.jsx";
+import { ExternalLink } from "../../../lib/components.tsx"
 
 export default ({ children, title, ...props }, filters) => {
+    const header = <header className="header_nav">
+        <div className="header_nav_inner">
+            <a href="/navostoke/"><img src="/img/navostoke/logotype.svg" alt="ナヴァストーケ トップページ" height="60" width="160" /></a>
+            <nav>
+                <ul className="header_nav_links">
+                    <li><a href="/navostoke/contact/">連絡先</a></li>
+                    <li><ExternalLink href="https://navostoke.booth.pm" icon={false}>通販・DL販売</ExternalLink></li>
+                </ul>
+            </nav>
+        </div>
+    </header>;
     const footer = <footer>
-        <p><a href="/navostoke/">サークルのトップページ</a> <a href="/">ふぁぼんのホームページ</a></p>
+        <p><a href="/">ふぁぼんのホームページ</a></p>
         <div id="footer">
             <p>© 2021-{new Date().getFullYear()} ナヴァストーケ «на востоке»</p>
         </div>
     </footer>;
     const fullTitle = `${title} | ナヴァストーケ`;
-    return defaultLayout({ children: [children, footer], ...props, bareTitle: true, title: fullTitle }, filters);
+    return defaultLayout({ children: [header, children, footer], ...props, bareTitle: true, title: fullTitle }, filters);
 }
