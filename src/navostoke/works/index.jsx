913685812b6644920933247a53968a0206c9281e
fabon <syobon.hinata.public@gmail.com>
2021-11-13 06:31:20 +0900

    Add "works" page

---
+++ src/navostoke/works/index.jsx
@@ -0,0 +1,18 @@
+import { ExternalLink } from "../../../lib/components.tsx"
+
+export const title = "作品一覧"
+export const description = "作品の一覧ページ"
+
+export default data => {
+    const books = data.series.filter(series => Array.isArray(series.books)).map(series =>
+        <div>
+            <h2>{series.name}</h2>
+            {series.description && <p>{series.description}</p>}
+            {series.books && series.books.map(book => <div><a href={`./${book.id}`}>{book.name}</a>{book.published || " (未発売)"}</div>)}
+        </div>
+    );
+    return <main role="main">
+        <h1>作品一覧</h1>
+        {books}
+    </main>;
+}
