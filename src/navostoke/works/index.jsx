c14df9baacf7de4666a6dda42eb5231bfd835ecd
fabon <syobon.hinata.public@gmail.com>
2021-11-16 23:34:38 +0900

    Update

--- src/navostoke/works/index.jsx
+++ src/navostoke/works/index.jsx
@@ -11,8 +11,20 @@ export default data => {
             {series.books && series.books.map(book => <div><a href={`./${book.id}/`}>{book.name}</a>{book.published || " (未発売)"}</div>)}
         </div>
     );
+    const collab = data.external_works.map(circle => {
+        const works = circle.works.map(book => <div>
+            <li>{book.url ? <ExternalLink href={book.url}>{book.name}</ExternalLink> : book.name} {book.description}</li>
+        </div>);
+        return <div>
+            <h3>{circle.name}</h3>
+            <ul>{works}</ul>
+        </div>;
+    });
     return <main role="main">
         <h1>作品一覧</h1>
         {books}
+
+        <h2>寄稿・制作協力</h2>
+        {collab}
     </main>;
 }
