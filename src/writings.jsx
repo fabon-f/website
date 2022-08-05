465baa87e009a6c7216c07bc28566e068fcdaf2a
fabon <syobon.hinata.public@gmail.com>
2022-08-05 21:07:42 +0900

    Update

--- src/writings.jsx
+++ src/writings.jsx
@@ -12,6 +12,7 @@ export default () => <main>
         <li>艦これ <ExternalLink href="https://www.pixiv.net/novel/show.php?id=17088933">「2月24日」</ExternalLink></li>
         <li>恋する小惑星 <ExternalLink href="https://www.pixiv.net/novel/show.php?id=16190901">「拝啓、海の向こうの空へ」</ExternalLink></li>
         <li>恋する小惑星 「ナナチカ探偵団と不可能な虹」(<ExternalLink href="https://utkiraracircle.github.io/koias">東京大学恋する小惑星同好会</ExternalLink>合同誌<ExternalLink href="https://utkiraracircle.github.io/posts/find-our-stars-2">『#FindOurStars Vol.2』</ExternalLink>寄稿)</li>
+        <li>初恋＊れ〜るとりっぷ 「初恋＊れ〜るみすてり」(<ExternalLink href="https://utkiraracircle.github.io">東京大学きらら同好会</ExternalLink>合同誌<ExternalLink href="https://utkiraracircle.github.io/posts/micare-2">『Micare Vol.2』</ExternalLink>寄稿)</li>
     </ul>
     <h2>評論</h2>
     <ul>
@@ -20,6 +21,7 @@ export default () => <main>
         <li><ExternalLink href="https://yuyusuki.hatenablog.com/entry/2022/06/12/190000">「東大生猪瀬舞概念」</ExternalLink> (<ExternalLink href="https://utkiraracircle.github.io/koias">東大恋する小惑星同好会</ExternalLink>合同誌<ExternalLink href="https://utkiraracircle.github.io/posts/find-our-stars-1">『#FindOurStars Vol.1』</ExternalLink>寄稿)</li>
         <li>「まちカドまぞく vs. ウクライナ語警察」 (<ExternalLink href="https://utkiraracircle.github.io">東京大学きらら同好会</ExternalLink>合同誌<ExternalLink href="https://utkiraracircle.github.io/posts/micare-1">『Micare Vol.1』</ExternalLink>寄稿)</li>
         <li>同人誌 <ExternalLink href="https://navostoke.fabon.info/works/russian-police-1/">『ロシア語警察24時 vol.1 人名エトセトラ』</ExternalLink></li>
+        <li>「那由多誰何の倫理学入門」 (<ExternalLink href="https://twitter.com/be_straighter">びストレーター</ExternalLink>様主催の合同誌<ExternalLink href="https://twitter.com/be_straighter/status/1555509013634289664">『まちカドまぞく評論合同 まちカドのお楽しみ』</ExternalLink>寄稿)</li>
     </ul>
     <h2>ブログ</h2>
     <p><a href="/library/">駄文図書館</a>参照。</p>
