f22356bd20b026b829649662de0e5db9ad53e4b3
fabon <syobon.hinata.public@gmail.com>
2023-03-20 00:35:12 +0900

    ブログ記事を追加

--- src/library.jsx
+++ src/library.jsx
@@ -23,6 +23,8 @@ export default data =>
                 </ul>
                 <li>小学生レベルの算数と理科で斜方投射を分析する方法。45度飛距離最長の証明もあるよ! <ExternalLink href="https://yuyusuki.hatenablog.com/entry/2022/08/24/183000">小学生でも分かる斜方投射</ExternalLink></li>
                 <li>京都大学艦これ同好会のブログできらら作品を布教(私は京大生ではない なぜ??) <ExternalLink href="https://kukancolle.hatenablog.com/entry/2022/04/24/192535">きらら作品を読もう!</ExternalLink></li>
+                <li>ぼっち・ざ・ろっく! 挿入歌の歌詞からアニメを考える <ExternalLink href="https://yuyusuki.hatenablog.com/entry/2022/12/06/184500">誰が「あのバンド」を書いたのか？</ExternalLink></li>
+                <li>ロシアンロック入門記事 <ExternalLink href="https://yuyusuki.hatenablog.com/entry/2022/12/23/144500">陰キャならロックを聴け! ロシア編</ExternalLink></li>
             </ul>
         </section>
 
@@ -49,11 +51,13 @@ export default data =>
             <ul>
                 <li>ハンドルネームの由来 <ExternalLink href="https://yuyusuki.hatenablog.com/entry/2020/10/29/192635">あなたのハンネはどこから? 私はクソゲーとTwitterから!</ExternalLink></li>
                 <li>自らの天才クールスレンダー美少女性癖の根源に対する考察 <ExternalLink href="https://yuyusuki.hatenablog.com/entry/2018/04/08/162259">灰原哀と拗らせた性癖</ExternalLink></li>
+                <li>天才少女が好きなマゾのキモ性癖を完璧に言語化! <ExternalLink href="https://yuyusuki.hatenablog.com/entry/2023/03/07/193000">ラプラスの悪魔に弄ばれたい</ExternalLink></li>
                 <li>小説中毒の私はいかにしてオタクになったか、あるいは私とエロゲソングのファーストコンタクト<br /><ExternalLink href="https://yuyusuki.hatenablog.com/entry/2018/04/30/000956">読書遍歴とSSとオタクの目覚め</ExternalLink></li>
                 <li>私がロシア語に出会った日 <ExternalLink href="https://yuyusuki.hatenablog.com/entry/2020/06/17/183743">学士の異常な愛情 または私は如何にして心配するのを止めてロシア語を愛するようになったか</ExternalLink></li>
                 <li>新元号「令和」発表記念、クール美少女とゆるふわ美少女の百合の尊さに関する考察<br /><ExternalLink href="https://yuyusuki.hatenablog.com/entry/2019/04/01/232956">私が特別尊いと思うキャラクター(特に女の子同士)の組み合わせについて</ExternalLink></li>
                 <li>受験ナメプ記録 <ExternalLink href="https://yuyusuki.hatenablog.com/entry/2020/05/30/230752">東大合格体験記 検閲なし版</ExternalLink></li>
                 <li>小学生の私が力学の斜方投射を分析したときの思考ログを記憶からサルベージ。小学生レベルゆえ三角関数すら出てこないので、三角関数不要論者にもおすすめ。 <ExternalLink href="https://yuyusuki.hatenablog.com/entry/2022/08/24/183000">小学生でも分かる斜方投射</ExternalLink></li>
+                <li><ExternalLink href="https://yuyusuki.hatenablog.com/entry/2023/01/09/183000">とある言語情報偏重オタクの世界観について</ExternalLink></li>
             </ul>
         </section>
 
@@ -69,6 +73,7 @@ export default data =>
                 <li>原作者(落合陽一氏)巡回済み <ExternalLink href="https://yuyusuki.hatenablog.com/entry/2021/12/20/015413">【サバの話だったんだよ】WEEKLY OCHIAI vs. ゆゆ式オタク</ExternalLink></li>
                 <li>革命歌を聞いて年越ししよう! <ExternalLink href="https://yuyusuki.hatenablog.com/entry/2022/01/01/213656">年越しの新定番「インターナショナル赤色歌合戦」のご提案</ExternalLink></li>
                 <li>恋する小惑星の猪瀬舞さんは東大生です。 <ExternalLink href="https://yuyusuki.hatenablog.com/entry/2022/06/12/190000"> 【全文公開】東大生猪瀬舞概念【#FindOurStars vol.1】 </ExternalLink></li>
+                <li>東大新入生向け二外ロシア語布教<strong>小説</strong> りろっしーもリローシャちゃんも登場するよ! <ExternalLink href="https://yuyusuki.hatenablog.com/entry/2023/03/10/133000">#二外はロシア語へ</ExternalLink></li>
             </ul>
         </section>
 
