7485442a4f1e0b038a40d1f2d17ad39c2e0b909a
fabon <syobon.hinata.public@gmail.com>
2023-07-08 03:43:27 +0900

    Update

--- src/index.jsx
+++ src/index.jsx
@@ -102,6 +102,7 @@ export default data =>
                 <tr><td>Twitter(大学用人格)</td><td><ExternalLink href="https://twitter.com/alisa_ut">@alisa_ut</ExternalLink></td></tr>
                 <tr><td>メール</td><td><a href="mailto:syobon.hinata.public@gmail.com">syobon.hinata.public@gmail.com</a></td></tr>
                 <tr><td>Discord</td><td>fabon</td></tr>
+                <tr><td>Mastodon (ActivityPub)</td><td><a rel="me" href="https://mstdn.poyo.me/@fabon">@fabon@mstdn.poyo.me </a></td></tr>
             </table>
         </section>
         <section>
